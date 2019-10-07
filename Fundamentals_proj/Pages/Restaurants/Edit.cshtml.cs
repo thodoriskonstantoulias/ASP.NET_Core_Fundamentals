303642c92061fd37fd14150576782b9e4edf6bc1
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-07 23:30:42 +0300

    Addition of edit razor page with binding

---
+++ Fundamentals_proj/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Food.Core;
+using Food.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Fundamentals_proj.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
