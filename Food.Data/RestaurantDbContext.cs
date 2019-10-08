15934dd80f58167ed471dbab057914505a49e48d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-08 23:15:30 +0300

    Packages downloaded and db context class created

---
+++ Food.Data/RestaurantDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Food.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
